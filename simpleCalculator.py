85905c685f9f03a5071172fad93d8b6e0fd333ac
NatPlatt <n.zollinger26@hotmail.com>
2020-05-04 21:29:59 -0600

    Update simpleCalculator.py

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -1,2 +1,6 @@
 #this is a simple calculator that I started in SoloLearn and finished myself
 
+while True:
+    print("Options:")
+    print("Enter 'add' to add two numbers")
+    print("Enter 'subtract' to subtract two numbers")
