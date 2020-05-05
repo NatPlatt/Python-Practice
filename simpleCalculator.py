ae2122c9f8eaa915fbdb031a14cc93eb777d1b70
NatPlatt <n.zollinger26@hotmail.com>
2020-05-04 21:31:07 -0600

    Update simpleCalculator.py

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -4,3 +4,6 @@ while True:
     print("Options:")
     print("Enter 'add' to add two numbers")
     print("Enter 'subtract' to subtract two numbers")
+    print("Enter 'multiply' to multiply two numbers")
+    print("Enter 'divide' to divide two numbers")
+    print("Enter 'quit' to end the program")
