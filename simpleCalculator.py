19f5fed4339d118dd03345b878e43d221ef7dc1c
NatPlatt <n.zollinger26@hotmail.com>
2020-05-04 21:33:35 -0600

    Update simpleCalculator.py

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -12,4 +12,8 @@ while True:
     
     if user_input == "quit":
         break
-    
+    elif user_input == "add":
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
+        result = str(num1 + num2)
+        print("The answer is" + result)
