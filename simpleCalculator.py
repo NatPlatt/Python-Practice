39984ed883a2de3385a35c0d9f6304d749fd106d
NatPlatt <n.zollinger26@hotmail.com>
2020-05-04 21:38:06 -0600

    Update simpleCalculator.py

--- simpleCalculator.py
+++ simpleCalculator.py
@@ -29,3 +29,9 @@ while True:
         num2 = float(input("Enter another number: "))
         result = str(num1 * num2)
         print("The answer is " + result)
+        
+    elif user_input == "divide":
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
+        result = str(num1 / num2)
+        print("The answer is " + result)
